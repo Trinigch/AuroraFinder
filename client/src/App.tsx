8d58a5e3c86544b2b044ef432f6f71e005c8057d
MrBalld <balld112205@gmail.com>
2024-11-05 17:03:06 -0600

    navBar work

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
-
+import { Outlet } from 'react-router-dom';
+import Nav from './components/Navbar'
 function App() {
 
   return (
     <>
-      
+      <header>
+        <h1>Aurora Finder</h1>
+        <Nav />
+      </header>
     </>
   )
 }
