a0f7487a629c5bb7560ad0f2f9a4986ee6911499
EthanForrestCarr <ethan@ethancarr.com>
2024-11-06 14:08:09 -0600

    imported request/response from express and corrected typescript errors in aurora-controller

--- server/src/controllers/aurora-controller.ts
+++ server/src/controllers/aurora-controller.ts
@@ -1,17 +1,28 @@
 import axios from 'axios';
+import { Request, Response } from 'express';
 
 export const getAuroraData = async (req: Request, res: Response) => {
   try {
-    const { lat, long } = req.query;
+    // Extract lat/long and ensure they are interpreted as numbers
+    const lat = parseFloat(req.query.lat as string);
+    const long = parseFloat(req.query.long as string);
+
+    if (isNaN(lat) || isNaN(long)) {
+      return res.status(400).json({ message: 'Invalid latitude or longitude' });
+    }
 
     const aurorasUrl = `http://api.auroras.live/v1/?type=all&lat=${lat}&long=${long}&ace=true&images=true&probability=true`;
 
-    const response = await axios.get(aurorasUrl);
+    const response = await axios.get<any>(aurorasUrl);
 
     res.json(response.data);
+
+    return;
 
   } catch (error) {
     console.error('Error fetching aurora data:', error);
     res.status(500).json({ message: 'Error fetching aurora data' });
+
+    return;
   }
 };
