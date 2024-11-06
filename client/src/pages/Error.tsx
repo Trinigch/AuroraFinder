8adea02cef940e5cb4dbab07781a4bea4a451eba
MrBalld <balld112205@gmail.com>
2024-11-06 11:24:13 -0600

    added content to landing page

--- client/src/pages/Error.tsx
+++ client/src/pages/Error.tsx
@@ -1,6 +1,11 @@
 
 const Error = () => {
-    
+    return (
+        <section>
+            <h1>404: Page Not Found Sorry</h1>
+            <h1> ¯\_(ツ)_/¯</h1>
+        </section>
+    );
 }
 
 
--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,6 +1,20 @@
 
 const Landing = () => {
-    
+    return (
+        <>
+        <h2>About Auroras</h2>
+        <section>
+            <h3>What Are Auroras?</h3>
+            <p>
+                Auroras also know as the Northern or Southern Lights are often described as ribons of mostly green light that can be observed by people near the poles.
+            </p>
+            <h3>How Are Auroras Made</h3>
+            <p>
+                Auroras form
+            </p>
+        </section>
+        </>
+    );
 }
 
 
