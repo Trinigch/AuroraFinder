633dbc26ae702f48db3085c3a138d4fa9abee298
MrBalld <balld112205@gmail.com>
2024-11-07 11:23:56 -0600

    login page seleton

--- client/src/pages/Find.tsx
+++ client/src/pages/Find.tsx
@@ -3,32 +3,25 @@ import React, { useState, useRef } from 'react';
 import '@googlemaps/extended-component-library/place_picker.js';
 import '@googlemaps/extended-component-library/overlay_layout.js';
 import { AdvancedMarker, Map, Pin, APIProvider } from '@vis.gl/react-google-maps';
-import { PlacePicker,  
-    PlaceReviews,
-    PlaceDataProvider,
-    PlaceDirectionsButton,
-    IconButton,
-    PlaceOverview,
-    SplitLayout,
-    OverlayLayout 
-} from '@googlemaps/extended-component-library/react';
+import { PlacePicker } from '@googlemaps/extended-component-library/react';
 import { PlacePicker as TPlacePicker } from '@googlemaps/extended-component-library/place_picker.js';
 import { OverlayLayout as TOverlayLayout } from '@googlemaps/extended-component-library/overlay_layout.js';
+import LocationInput from '../components/LocationInput';
 
 const Find = () => {
 
-    const pickerRef = useRef<TPlacePicker>(null);
-    //@ts-ignore
-    const [userLocation, setUserLocation] = useState <google.maps.places.Place | undefined>(undefined);
-
     return(
         <>
-            <div id="place-picker-box">
-                <div id="place-picker-container">
-                    <PlacePicker className=''/>
-                    <input id="selected-address" type="hidden" name="address" />
+            <section>
+                
+                <div className='container'>
+                    <h3>Your Areas Aurora Data</h3>
+                    <img src="https://services.swpc.noaa.gov/images/animations/ovation/north/latest.jpg" alt='todays aurora data as an img from NOAA' width={400} height={400}/>
+                    <p>Aurora intensity: </p>
                 </div>
-            </div>
+            </section>
+        </>
+
 
 {/* 
 
@@ -44,7 +37,6 @@ const Find = () => {
                 <input type="text" id="state" name="state"/><br/>
                 <input type="submit" className="submitButton">Submit</input>
             </form> */}
-        </>
     );
 }
 
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,10 +1,43 @@
+import { useState } from "react";
 
 const Login = () => {
-    return (
-        <>
-            <form >
 
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleInputChange = (e) => {
+        // Getting the value and name of the input which triggered the change
+        const { name, value } = e.target;
+    
+        return name === 'userName' ? setUserName(value) : setPassword(value);
+    };
+
+
+    return (
+        <> 
+            <div className="conatainer">
+            <form action="">
+                <label htmlFor='userName'>UserName:</label>
+                <input 
+                type="text" 
+                name="userName"
+                id="userName"
+                onChange={handleInputChange}
+                />
+                <input 
+                type="text"
+                name="password"
+                id="password"
+                onChange={handleInputChange}
+                />
+                <button type="submit">
+                    Login
+                </button>
+                <button type="submit">
+                    Sign Up
+                </button>
             </form>
+            </div>
         </>
     );
 }
